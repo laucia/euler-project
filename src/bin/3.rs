047e26094fa90228e845d5a8dfb09f408e302b44
Lauris Jullien <lauris.jullien@telecom-paristech.org>
2015-04-05 17:12:56 +0200

    Working problem 3

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -2,12 +2,14 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
-extern crate core;
+extern crate num;
+
+use num::integer::Integer;
 
 fn is_prime(value: u64, cache: &mut Vec<u64>) -> bool{
     let top_limit = ((value-1) as f64).sqrt().ceil() as u64;
-    for prime in cache {
-        if value % prime == 0 {
+    for prime in cache.iter() {
+        if value.is_multiple_of(&prime) {
             return false;
         }
     }
@@ -17,7 +19,7 @@ fn is_prime(value: u64, cache: &mut Vec<u64>) -> bool{
     };
 
     for i in start..top_limit {
-        if value % i == 0 {
+        if value.is_multiple_of(&i) {
             return false;
         }
     }
@@ -26,21 +28,27 @@ fn is_prime(value: u64, cache: &mut Vec<u64>) -> bool{
 }
 
 fn biggest_prime_naive(value: u64) -> u64 {
+    let mut temp_value = value;
     let mut cache = Vec::new();
     let mut prime_divisors = Vec::new();
 
-    let mut i = 0;
-    for i in 2..value {
+    let mut i = 2;
+    while temp_value > 1 {
         if is_prime(i, &mut cache) {
-            while value % i == 0 {
-                value /= i;
+            while temp_value.is_multiple_of(&i) {
+                temp_value /= i;
+                prime_divisors.push(i);
             }
-            println!("{:?}", value);
         }
+        i += 1;
     }
-    0
+    println!("{:?}", prime_divisors);
+    match prime_divisors.len() {
+        0 => 0,
+        n => prime_divisors[n-1],
+    }
 }
 
 fn main() {
-    println!("{:?}", biggest_prime_naive(10000));
+    println!("{:?}", biggest_prime_naive(600851475143));
 }
