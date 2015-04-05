2f9eacb0ed1fcc2f3db0e8edf1f631837eacf72b
Lauris Jullien <lauris.jullien@telecom-paristech.org>
2015-04-05 15:57:32 +0200

    problem 3 WIP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # euler-project
+
+run with `cargo run --bin problem_XXXX`
--- src/bin/problem_0003.rs
+++ src/bin/problem_0003.rs
@@ -2,30 +2,48 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
-use std::num;
+extern crate core;
 
-fn is_prime(value: u64, cache: &mut[u64]){
-    let top_limit = num::sqrt(value-1).ceil()
+use std::num;
+use core::ops::Rem;
+
+fn is_prime(value: u64, cache: &mut Vec<u64>) -> bool{
+    let top_limit = ((value-1) as f64).sqrt().ceil() as u64;
     for prime in cache {
-        if i % prime == 0 {
+        if value % prime == 0 {
             return false;
         }
     }
+    let start = match cache.len() {
+        0 => 2,
+        n => cache[n-1],
+    };
+
+    for i in start..top_limit {
+        if value % i == 0 {
+            return false;
+        }
+    }
+    cache.push(value);
+    return true;
 }
 
 fn biggest_prime_naive(value: u64) -> u64 {
-    let mut vector = Vec::new()
-    let mut is_prime;
-    for i in 2...top_limit {
-        is_prime = false;
-        a = for prime in vector {
-            if i % prime == 0 {
-                break;
-            }
-        }
-        if a.peek().is_none() {
-            vector.append(i)
+    let mut cache = Vec::new();
+    let mut prime_divisors = Vec::new();
 
+    let mut i = 0;
+    for i in 2..value {
+        if is_prime(i, &mut cache) {
+            while value % i == 0 {
+                value /= i;
+            }
+            println!("{:?}", value);
         }
     }
+    0
+}
+
+fn main() {
+    println!("{:?}", biggest_prime_naive(10000));
 }
