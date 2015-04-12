0674a23c3f61d9cb7a976add88438899654f00e9
Lauris Jullien <lauris.jullien@telecom-paristech.org>
2015-04-12 19:19:31 +0200

    Problem 18

--- common/algorithm/src/triangular.rs
+++ common/algorithm/src/triangular.rs
@@ -1,5 +1,9 @@
 /// Some utility around triangular numbers
+
+use std::cmp::Ord;
+use std::cmp;
 use std::mem;
+use std::ops::Add;
 
 // Triangular numbers sequence iterator.
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -49,6 +53,56 @@ pub fn reverse_triangular(n: u64) -> Result<u64, String> {
     }
 }
 
+// Triangle Of Number
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+struct NumberTriangle<T> {
+    numbers: Vec<T>,
+    side: usize,
+}
+
+impl<T: Clone> NumberTriangle<T> {
+    fn new(numbers: Vec<T>) -> NumberTriangle<T> {
+        let length = numbers.len();
+        NumberTriangle{
+            numbers: numbers,
+            side : match reverse_triangular(length as u64) {
+                Ok(x) => x as usize,
+                Err(msg) => panic!(msg),
+            }
+        }
+    }
+
+    fn get(&self, line: usize, col: usize) -> T {
+        assert!(line < self.side && col < self.side);
+
+        let offset = nth_triangular_number(line as u64) as usize;
+        self.numbers[col + offset].clone()
+    }
+
+    fn set(&mut self, line: usize, col: usize, value: T) {
+        assert!(line < self.side && col < self.side);
+
+        let offset = nth_triangular_number(line as u64) as usize;
+        self.numbers[col + offset] = value;
+    }
+}
+
+pub fn solve_triangle_max_path<T: Add<T, Output = T> + Clone + Ord>(triangle_number: &[T]) -> T {
+    let mut trinum : NumberTriangle<T> = NumberTriangle::new(triangle_number.iter().map(|x| x.clone()).collect());
+    let mut new_value;
+    for line in (1 .. trinum.side).rev() {
+        for col in (0 .. line) {
+            new_value = trinum.get(line - 1, col) + cmp::max(
+                trinum.get(line, col),
+                trinum.get(line, col + 1)
+            );
+            trinum.set(line - 1, col, new_value);
+        }
+    }
+    trinum.get(0, 0)
+}
+
 // Tests
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -69,4 +123,14 @@ fn reverse_triangular_test() {
     assert_eq!(7, reverse_triangular(28).unwrap());
     assert_eq!(8, reverse_triangular(nth_triangular_number(8)).unwrap());
     assert!(reverse_triangular(27).is_err());
+}
+
+#[test]
+fn solve_triangle_max_path_test() {
+    assert_eq!(23, solve_triangle_max_path(&[
+        3,
+        7, 4,
+        2, 4, 6,
+        8, 5, 9, 3,
+    ]));
 }
